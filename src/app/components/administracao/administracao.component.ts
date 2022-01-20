95a68de45dab38d110120831783b8c943a0ffa3c
leoglima <leoglima@gmail.com>
2022-01-20 12:02:47 -0300

    ajustes pré produção 2

--- src/app/components/administracao/administracao.component.ts
+++ src/app/components/administracao/administracao.component.ts
@@ -21,13 +21,19 @@ export class AdministracaoComponent implements OnInit {
   }
 
   async exportarBase() {
+    this.spinner = 5;
     this._snackBar.open("Carregando, aguarde!", "", { duration: 5000, panelClass: ["snack-vermelho"] });
     const wb: XLSX.WorkBook = XLSX.utils.book_new();
     await this.fs.carregarBaseCompleta();
+    this.spinner = 10;
     await this.fs.getBancosDocs();
+    this.spinner = 15;
     await this.fs.getCargosDocs();
+    this.spinner = 20;
     await this.fs.getServicosDocs();
+    this.spinner = 25;
     await this.fs.getLogsDocs();
+    this.spinner = 30;
     const listaCondominios = this.fs.listaExportCondominios;
     const listaFuncionarios = this.fs.listaExportFuncionarios;
     const listaFornecedores = this.fs.listaExportFornecedores;
@@ -37,27 +43,38 @@ export class AdministracaoComponent implements OnInit {
     const listaCargos = this.fs.listaExportCargos;
     const listaUsers = this.fs.listaExportUsers;
     const listaLogs = this.fs.listaExportLogs;
-
+    this.spinner = 35;
     const wsCondominios: XLSX.WorkSheet = XLSX.utils.json_to_sheet(listaCondominios);
     XLSX.utils.book_append_sheet(wb, wsCondominios, 'Condominios');
+    this.spinner = 40;
     const wsFuncionarios: XLSX.WorkSheet = XLSX.utils.json_to_sheet(listaFuncionarios);
     XLSX.utils.book_append_sheet(wb, wsFuncionarios, 'Funcionarios');
+    this.spinner = 45;
     const wscFornecedores: XLSX.WorkSheet = XLSX.utils.json_to_sheet(listaFornecedores);
     XLSX.utils.book_append_sheet(wb, wscFornecedores, 'Fornecedores');
+    this.spinner = 50;
     const wscondominos: XLSX.WorkSheet = XLSX.utils.json_to_sheet(listaCondominos);
     XLSX.utils.book_append_sheet(wb, wscondominos, 'Condominos');
+    this.spinner = 60;
     const wsServicos: XLSX.WorkSheet = XLSX.utils.json_to_sheet(listaServicos);
     XLSX.utils.book_append_sheet(wb, wsServicos, 'Servicos');
+    this.spinner = 70;
     const wsBancos: XLSX.WorkSheet = XLSX.utils.json_to_sheet(listaBancos);
     XLSX.utils.book_append_sheet(wb, wsBancos, 'Bancos');
+    this.spinner = 75;
     const wsCargos: XLSX.WorkSheet = XLSX.utils.json_to_sheet(listaCargos);
     XLSX.utils.book_append_sheet(wb, wsCargos, 'Cargos');
+    this.spinner = 80;
     const wsUsers: XLSX.WorkSheet = XLSX.utils.json_to_sheet(listaUsers);
     XLSX.utils.book_append_sheet(wb, wsUsers, 'Users');
+    this.spinner = 90;
     const wsLogs: XLSX.WorkSheet = XLSX.utils.json_to_sheet(listaLogs);
     XLSX.utils.book_append_sheet(wb, wsLogs, 'Logs');
     /* save to file */
+    
+    this.spinner = 95;
     XLSX.writeFile(wb, this.fileName);
+    this.spinner = 0;
   }
 
   

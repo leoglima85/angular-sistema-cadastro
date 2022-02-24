fe74a0bddfddaf28b05e8b372d09b83d71d46363
leoglima <leoglima@gmail.com>
2022-02-24 16:04:36 -0300

    criação inicial do sistema de controle 2

--- src/app/components/controle/controle.component.ts
+++ src/app/components/controle/controle.component.ts
@@ -18,22 +18,8 @@ export class ControleComponent implements OnInit {
   listaContratos: any[] = [];
   notas: any[] = [];
 
-  // contratos = [
-  //   // { condominio: "E5KJiJ2z5v3YMuPyaKul", dacc: true, dataVencimento: 1, fornecedor: "51IQ9wAqgPwNXbEJX6W2", nome: "0O9J90X6lMpx6nQ4LqBn", numeroContrato: "D034KFD87", observacao: "sem obs", periodicidade: "Mensal" },
-  //   // { condominio: "DJOlrSQCSPnnT1vqHPYK", dacc: true, dataVencimento: 5, fornecedor: "51IQ9wAqgPwNXbEJX6W2", nome: "0O9J90X6lMpx6nQ4LqBn", numeroContrato: "DSK30FH54", observacao: "sem obs", periodicidade: "Anual" }
-  // ];
+  constructor(private fs: FirestoreService) {
 
-  // notas = [
-  //   { contrato: "D034KFD87", competencia: 1, dataVencimento: "10/1/2022", valor: 123.45, recebido: "nao", obs: "" },
-  //   // { contrato: "DSK30FH54", competencia: 2, dataVencimento: "11/2/2022", valor: 123.45, recebido: "nao", obs: "" },
-  //   // { contrato: "D034KFD87", competencia: 3, dataVencimento: "12/3/2022", valor: 123.45, recebido: "nao", obs: "" },
-  //   // { contrato: "DSK30FH54", competencia: 4, dataVencimento: "13/4/2022", valor: 123.45, recebido: "nao", obs: "" },
-  // ];
-
-
-  constructor(private fs : FirestoreService) {
-    
-    
   }
 
   async ngOnInit(): Promise<void> {
@@ -41,8 +27,6 @@ export class ControleComponent implements OnInit {
     this.listaContratos = this.fs.listaContratos
     await this.fs.getNotasDocs()
     this.notas = this.fs.listaNotas
-    // console.log(this.listaContratos)
-    // console.log("ntas: ",this.notas)
     let busca = false
     let mes = new Date().getMonth() + 1
     let mesAnterior = mes - 1
@@ -52,7 +36,6 @@ export class ControleComponent implements OnInit {
     let anoAnterior = ano - 1
     let anoSeguinte = ano + 1
     this.listaContratos.forEach(doc => {
-      // console.log("doc: ", doc.numeroContrato, "mes: ",mes)
       if (mes > 1 && mes < 11) {
         console.log(">1<11.", mes)
         busca = false
@@ -61,20 +44,15 @@ export class ControleComponent implements OnInit {
             busca = true
         })
         if (!busca) {
-          this.fs.addNotaDoc({contrato: doc.numeroContrato,
+          this.fs.addNotaDoc({
+            contrato: doc.numeroContrato,
             competencia: mesAnterior,
             dataVencimento: `${doc.dataVencimento}/${mesAnterior}/${ano}`,
             valor: 0,
             recebido: "nao",
-            obs: ""})
-          // this.notas.push({
-          //   contrato: doc.numeroContrato,
-          //   competencia: mesAnterior,
-          //   dataVencimento: `${doc.dataVencimento}/${mesAnterior}/${ano}`,
-          //   valor: 0,
-          //   recebido: "nao",
-          //   obs: ""
-          // })
+            obs: ""
+          })
+
         }
         busca = false
         this.notas.find(nota => {
@@ -82,20 +60,15 @@ export class ControleComponent implements OnInit {
             busca = true
         })
         if (!busca) {
-          this.fs.addNotaDoc({contrato: doc.numeroContrato,
+          this.fs.addNotaDoc({
+            contrato: doc.numeroContrato,
             competencia: mes,
             dataVencimento: `${doc.dataVencimento}/${mes}/${ano}`,
             valor: 0,
             recebido: "nao",
-            obs: ""})
-          // this.notas.push({
-          //   contrato: doc.numeroContrato,
-          //   competencia: mes,
-          //   dataVencimento: `${doc.dataVencimento}/${mes}/${ano}`,
-          //   valor: 0,
-          //   recebido: "nao",
-          //   obs: ""
-          // })
+            obs: ""
+          })
+
         }
         busca = false
         this.notas.find(nota => {
@@ -103,20 +76,15 @@ export class ControleComponent implements OnInit {
             busca = true
         })
         if (!busca) {
-          this.fs.addNotaDoc({contrato: doc.numeroContrato,
+          this.fs.addNotaDoc({
+            contrato: doc.numeroContrato,
             competencia: mesSeguinte1,
             dataVencimento: `${doc.dataVencimento}/${mesSeguinte1}/${ano}`,
             valor: 0,
             recebido: "nao",
-            obs: ""})
-          // this.notas.push({
-          //   contrato: doc.numeroContrato,
-          //   competencia: mesSeguinte1,
-          //   dataVencimento: `${doc.dataVencimento}/${mesSeguinte1}/${ano}`,
-          //   valor: 0,
-          //   recebido: "nao",
-          //   obs: ""
-          // })
+            obs: ""
+          })
+
         }
         busca = false
         this.notas.find(nota => {
@@ -124,20 +92,15 @@ export class ControleComponent implements OnInit {
             busca = true
         })
         if (!busca) {
-          this.fs.addNotaDoc({contrato: doc.numeroContrato,
+          this.fs.addNotaDoc({
+            contrato: doc.numeroContrato,
             competencia: mesSeguinte2,
             dataVencimento: `${doc.dataVencimento}/${mesSeguinte2}/${ano}`,
             valor: 0,
             recebido: "nao",
-            obs: ""})
-          // this.notas.push({
-          //   contrato: doc.numeroContrato,
-          //   competencia: mesSeguinte2,
-          //   dataVencimento: `${doc.dataVencimento}/${mesSeguinte2}/${ano}`,
-          //   valor: 0,
-          //   recebido: "nao",
-          //   obs: ""
-          // })
+            obs: ""
+          })
+
         }
       } else if (mes == 1) {
         mesAnterior = 12
@@ -150,20 +113,15 @@ export class ControleComponent implements OnInit {
             busca = true
         })
         if (!busca) {
-          this.fs.addNotaDoc({contrato: doc.numeroContrato,
+          this.fs.addNotaDoc({
+            contrato: doc.numeroContrato,
             competencia: mesAnterior,
             dataVencimento: `${doc.dataVencimento}/${mesAnterior}/${anoAnterior}`,
             valor: 0,
             recebido: "nao",
-            obs: ""})
-          // this.notas.push({
-          //   contrato: doc.numeroContrato,
-          //   competencia: mesAnterior,
-          //   dataVencimento: `${doc.dataVencimento}/${mesAnterior}/${anoAnterior}`,
-          //   valor: 0,
-          //   recebido: "nao",
-          //   obs: ""
-          // })
+            obs: ""
+          })
+
         }
         busca = false
         this.notas.find(nota => {
@@ -171,20 +129,15 @@ export class ControleComponent implements OnInit {
             busca = true
         })
         if (!busca) {
-          this.fs.addNotaDoc({contrato: doc.numeroContrato,
+          this.fs.addNotaDoc({
+            contrato: doc.numeroContrato,
             competencia: mes,
             dataVencimento: `${doc.dataVencimento}/${mes}/${ano}`,
             valor: 0,
             recebido: "nao",
-            obs: ""})
-          // this.notas.push({
-          //   contrato: doc.numeroContrato,
-          //   competencia: mes,
-          //   dataVencimento: `${doc.dataVencimento}/${mes}/${ano}`,
-          //   valor: 0,
-          //   recebido: "nao",
-          //   obs: ""
-          // })
+            obs: ""
+          })
+
         }
         busca = false
         this.notas.find(nota => {
@@ -192,20 +145,15 @@ export class ControleComponent implements OnInit {
             busca = true
         })
         if (!busca) {
-          this.fs.addNotaDoc({contrato: doc.numeroContrato,
+          this.fs.addNotaDoc({
+            contrato: doc.numeroContrato,
             competencia: mesSeguinte1,
             dataVencimento: `${doc.dataVencimento}/${mesSeguinte1}/${ano}`,
             valor: 0,
             recebido: "nao",
-            obs: ""})
-          // this.notas.push({
-          //   contrato: doc.numeroContrato,
-          //   competencia: mesSeguinte1,
-          //   dataVencimento: `${doc.dataVencimento}/${mesSeguinte1}/${ano}`,
-          //   valor: 0,
-          //   recebido: "nao",
-          //   obs: ""
-          // })
+            obs: ""
+          })
+
         }
         busca = false
         this.notas.find(nota => {
@@ -213,20 +161,15 @@ export class ControleComponent implements OnInit {
             busca = true
         })
         if (!busca) {
-          this.fs.addNotaDoc({contrato: doc.numeroContrato,
+          this.fs.addNotaDoc({
+            contrato: doc.numeroContrato,
             competencia: mesSeguinte2,
             dataVencimento: `${doc.dataVencimento}/${mesSeguinte2}/${ano}`,
             valor: 0,
             recebido: "nao",
-            obs: ""})
-          // this.notas.push({
-          //   contrato: doc.numeroContrato,
-          //   competencia: mesSeguinte2,
-          //   dataVencimento: `${doc.dataVencimento}/${mesSeguinte2}/${ano}`,
-          //   valor: 0,
-          //   recebido: "nao",
-          //   obs: ""
-          // })
+            obs: ""
+          })
+
         }
       } else if (mes == 11) {
         mesSeguinte2 = 1
@@ -237,20 +180,15 @@ export class ControleComponent implements OnInit {
             busca = true
         })
         if (!busca) {
-          this.fs.addNotaDoc({contrato: doc.numeroContrato,
+          this.fs.addNotaDoc({
+            contrato: doc.numeroContrato,
             competencia: mesAnterior,
             dataVencimento: `${doc.dataVencimento}/${mesAnterior}/${ano}`,
             valor: 0,
             recebido: "nao",
-            obs: ""})
-          // this.notas.push({
-          //   contrato: doc.numeroContrato,
-          //   competencia: mesAnterior,
-          //   dataVencimento: `${doc.dataVencimento}/${mesAnterior}/${ano}`,
-          //   valor: 0,
-          //   recebido: "nao",
-          //   obs: ""
-          // })
+            obs: ""
+          })
+
         }
         busca = false
         this.notas.find(nota => {
@@ -258,20 +196,15 @@ export class ControleComponent implements OnInit {
             busca = true
         })
         if (!busca) {
-          this.fs.addNotaDoc({contrato: doc.numeroContrato,
+          this.fs.addNotaDoc({
+            contrato: doc.numeroContrato,
             competencia: mes,
             dataVencimento: `${doc.dataVencimento}/${mes}/${ano}`,
             valor: 0,
             recebido: "nao",
-            obs: ""})
-          // this.notas.push({
-          //   contrato: doc.numeroContrato,
-          //   competencia: mes,
-          //   dataVencimento: `${doc.dataVencimento}/${mes}/${ano}`,
-          //   valor: 0,
-          //   recebido: "nao",
-          //   obs: ""
-          // })
+            obs: ""
+          })
+
         }
         busca = false
         this.notas.find(nota => {
@@ -279,20 +212,15 @@ export class ControleComponent implements OnInit {
             busca = true
         })
         if (!busca) {
-          this.fs.addNotaDoc({contrato: doc.numeroContrato,
+          this.fs.addNotaDoc({
+            contrato: doc.numeroContrato,
             competencia: mesSeguinte1,
             dataVencimento: `${doc.dataVencimento}/${mesSeguinte1}/${ano}`,
             valor: 0,
             recebido: "nao",
-            obs: ""})
-          // this.notas.push({
-          //   contrato: doc.numeroContrato,
-          //   competencia: mesSeguinte1,
-          //   dataVencimento: `${doc.dataVencimento}/${mesSeguinte1}/${ano}`,
-          //   valor: 0,
-          //   recebido: "nao",
-          //   obs: ""
-          // })
+            obs: ""
+          })
+
         }
         busca = false
         this.notas.find(nota => {
@@ -300,20 +228,15 @@ export class ControleComponent implements OnInit {
             busca = true
         })
         if (!busca) {
-          this.fs.addNotaDoc({contrato: doc.numeroContrato,
+          this.fs.addNotaDoc({
+            contrato: doc.numeroContrato,
             competencia: mesSeguinte2,
             dataVencimento: `${doc.dataVencimento}/${mesSeguinte2}/${anoSeguinte}`,
             valor: 0,
             recebido: "nao",
-            obs: ""})
-          // this.notas.push({
-          //   contrato: doc.numeroContrato,
-          //   competencia: mesSeguinte2,
-          //   dataVencimento: `${doc.dataVencimento}/${mesSeguinte2}/${anoSeguinte}`,
-          //   valor: 0,
-          //   recebido: "nao",
-          //   obs: ""
-          // })
+            obs: ""
+          })
+
         }
       } else if (mes == 12) {
         mesSeguinte1 = 1
@@ -325,20 +248,15 @@ export class ControleComponent implements OnInit {
             busca = true
         })
         if (!busca) {
-          this.fs.addNotaDoc({contrato: doc.numeroContrato,
+          this.fs.addNotaDoc({
+            contrato: doc.numeroContrato,
             competencia: mesAnterior,
             dataVencimento: `${doc.dataVencimento}/${mesAnterior}/${ano}`,
             valor: 0,
             recebido: "nao",
-            obs: ""})
-          // this.notas.push({
-          //   contrato: doc.numeroContrato,
-          //   competencia: mesAnterior,
-          //   dataVencimento: `${doc.dataVencimento}/${mesAnterior}/${ano}`,
-          //   valor: 0,
-          //   recebido: "nao",
-          //   obs: ""
-          // })
+            obs: ""
+          })
+
         }
         busca = false
         this.notas.find(nota => {
@@ -346,20 +264,15 @@ export class ControleComponent implements OnInit {
             busca = true
         })
         if (!busca) {
-          this.fs.addNotaDoc({contrato: doc.numeroContrato,
+          this.fs.addNotaDoc({
+            contrato: doc.numeroContrato,
             competencia: mes,
             dataVencimento: `${doc.dataVencimento}/${mes}/${ano}`,
             valor: 0,
             recebido: "nao",
-            obs: ""})
-          // this.notas.push({
-          //   contrato: doc.numeroContrato,
-          //   competencia: mes,
-          //   dataVencimento: `${doc.dataVencimento}/${mes}/${ano}`,
-          //   valor: 0,
-          //   recebido: "nao",
-          //   obs: ""
-          // })
+            obs: ""
+          })
+
         }
         busca = false
         this.notas.find(nota => {
@@ -367,59 +280,56 @@ export class ControleComponent implements OnInit {
             busca = true
         })
         if (!busca) {
-          this.fs.addNotaDoc({contrato: doc.numeroContrato,
+          this.fs.addNotaDoc({
+            contrato: doc.numeroContrato,
             competencia: mesSeguinte1,
             dataVencimento: `${doc.dataVencimento}/${mesSeguinte1}/${anoSeguinte}`,
             valor: 0,
             recebido: "nao",
-            obs: ""})
-          // this.notas.push({
-          //   contrato: doc.numeroContrato,
-          //   competencia: mesSeguinte1,
-          //   dataVencimento: `${doc.dataVencimento}/${mesSeguinte1}/${anoSeguinte}`,
-          //   valor: 0,
-          //   recebido: "nao",
-          //   obs: ""
-          // })
+            obs: ""
+          })
+
         }
         busca = false
         this.notas.find(nota => {
           if (nota.contrato === doc.numeroContrato && (mesSeguinte2) == nota.competencia)
             busca = true
-          
+
         })
         if (!busca) {
-          this.fs.addNotaDoc({contrato: doc.numeroContrato,
+          this.fs.addNotaDoc({
+            contrato: doc.numeroContrato,
             competencia: mesSeguinte2,
             dataVencimento: `${doc.dataVencimento}/${mesSeguinte2}/${anoSeguinte}`,
             valor: 0,
             recebido: "nao",
-            obs: ""})
-          // this.notas.push({
-          //   contrato: doc.numeroContrato,
-          //   competencia: mesSeguinte2,
-          //   dataVencimento: `${doc.dataVencimento}/${mesSeguinte2}/${anoSeguinte}`,
-          //   valor: 0,
-          //   recebido: "nao",
-          //   obs: ""
-          // })
+            obs: ""
+          })
+
         }
       }
 
     })
     this.notas.sort((a, b) => { return a.competencia - b.competencia })
-  }
-
-  ngAfterViewInit() {
-    //this.load()
-    
-  }
-
-  load(){
-    // console.log("load")
+    // this.load()
     this.dataSource = new MatTableDataSource(this.notas);
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
+    setTimeout(() => {
+      console.log("5 segundos depois")
+
+    }, 5000)
+  }
+
+  ngAfterViewInit() {
+
+
+  }
+
+  load() {
+    console.log("carregar")
+
+
   }
 
   applyFilter(event: Event) {
